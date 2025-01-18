c621617dda300aa3ef0406248ae9af7a11084877
Asam <abbasaliaboubakar@gmail.com>
2025-01-18 13:22:10 +0100

    feat: about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
-import Footer from "@/components/Footer";
+import About from "@/components/About";
 import Header from "@/components/Header";
 import DefaultLayout from "@/layouts/default.layout";
-import { NunitoUiDisplay, SkylarSansBoldUiDisplay } from "@/lib/fonts";
-import cn from "clsx";
 
 const Home = () => {
   return (
@@ -10,29 +8,9 @@ const Home = () => {
       <DefaultLayout className="py-[111px]">
         <div className="container mx-auto">
           <Header />
-          <p
-            className={`${cn(
-              NunitoUiDisplay.variable,
-              NunitoUiDisplay.className
-            )}`}
-          >
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit
-            incidunt minus nesciunt pariatur laudantium, dolore impedit
-            dignissimos dolorum, accusamus quidem veniam excepturi aperiam
-            saepe, veritatis quibusdam amet repudiandae ducimus harum maiores
-            assumenda voluptates? Corrupti expedita illo quis provident ex
-            explicabo dolor fugit! Assumenda, soluta numquam totam accusantium
-            nemo illum veniam voluptatibus perferendis earum aliquam incidunt
-            voluptate fugit dolorem pariatur nihil asperiores, beatae itaque
-            quos sunt! Quod officiis dicta deserunt culpa nemo sunt! Facilis
-            tempora nemo deleniti blanditiis impedit cupiditate iste magni quam
-            corrupti numquam quia iure alias quas facere, cumque veritatis
-            consectetur quis doloribus quos deserunt earum inventore voluptas
-            est!
-          </p>
+          <About className="mt-[40px] sm:mt-[60px] lg:mt-[120px] xl:mt-[165px] mb-[40px]" />
         </div>
       </DefaultLayout>
-      <Footer />
     </div>
   );
 };
