adfce432c967c2e098acc80ec8fe00f11048a2fb
Asam <abbasaliaboubakar@gmail.com>
2025-01-24 09:49:48 +0100

    slide to rigth

---
+++ src/Animations/SlideRigth/index.tsx
@@ -0,0 +1,44 @@
+import { motion, useInView } from "framer-motion";
+import { ReactNode, useRef } from "react";
+
+interface SlideRigthProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+  delay?: number;
+}
+
+const SlideRight = ({
+  children,
+  delay = 0,
+  className,
+  ...props
+}: SlideRigthProps) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const slideRightVariants = {
+    hidden: { x: -50, opacity: 0 },
+    visible: { x: 0, opacity: 1 },
+    exit: { x: 50, opacity: 0 },
+  };
+
+  return (
+    <motion.div
+      variants={slideRightVariants}
+      initial="hidden"
+      className={className}
+      animate={isInView ? "visible" : "hidden"}
+      exit="exit"
+      transition={{
+        duration: 0.6,
+        ease: "easeOut",
+        delay: delay,
+      }}
+      ref={ref}
+    >
+      <div {...props}></div>
+      {children}
+    </motion.div>
+  );
+};
+
+export default SlideRight;
